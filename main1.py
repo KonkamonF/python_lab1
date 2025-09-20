af62425730b0d2097b0d569da0e220221cbe072f
KonkamonF <konkamonfungsuk@gmail.com>
2025-09-20 13:58:03 +0700

    python

--- main1.py
+++ main1.py
@@ -1,1 +1,6 @@
-print("konkamon")
+import studentgrade
+
+
+studentgrade.print_mult_table(5, 20)
+studentgrade.print(s["name"])
+studentgrade.print("นักเรียนที่ได้ A มี", count,"คน")
---
+++ studentgrade.py
@@ -0,0 +1,19 @@
+def print_mult_table(a, b):
+    for i in range(a, b+1):
+        for j in range(1, 13):
+            if (i * j) % 2 == 0:
+                print(f"{i} x {j} = {i*j}")
+
+students = [
+    {'name': 'Ploy', 'grade': 'A'},
+    {'name': 'Ploy1', 'grade': 'B'},
+    {'name': 'Ploy2', 'grade': 'A'},
+    {'name': 'Ploy3', 'grade': 'A'}
+]
+print(students)
+for s in students:
+ if s["grade"] == "A":
+   
+
+    count = sum(1 for s in students if s["grade"] == "A")
+
